7828c706048e533637df2962b438f946c0c4aac6
吴官林 <wuguanlin@wuguanlindeMBP.lan>
2024-08-07 17:18:17 +0800

    init to respository

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ import os
 from typing import List, Optional
 
 import aiohttp
+import requests
+import logging
 from fastapi import FastAPI, HTTPException, BackgroundTasks, UploadFile, File, Form
 from pydantic import BaseModel
 
 from magic_pdf_parse_main import pdf_parse_main
 from magic_pdf.model.doc_analyze_by_custom_model import ModelSingleton
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 app = FastAPI()
 
@@ -29,6 +33,7 @@ class FileInfo(BaseModel):
 class FileList(BaseModel):
     file_list: List[FileInfo]
     token: str
+    strategy: str = "fast"  # 可能接收的解析模式
 
 
 class ResponseModel(BaseModel):
@@ -59,6 +64,7 @@ async def analysis(
     """
     try:
         file_list_data = eval(file_list)
+        file_list_data.update({"strategy": file_list_data.get("strategy", "fast")})
         file_list_obj = FileList(**file_list_data)
 
         if not validate_token(file_list_obj.token):
@@ -107,6 +113,7 @@ async def post_pdf_parse_main(file_info, pdf_path):
 
     """
     result = ""
+    exist_images = False
     try:
         pdf_name = os.path.basename(pdf_path).split(".")[0]
         pdf_path_parent = os.path.dirname(pdf_path)
@@ -114,26 +121,30 @@ async def post_pdf_parse_main(file_info, pdf_path):
         output_path = os.path.join(pdf_path_parent, pdf_name)
 
         output_image_path = os.path.join(output_path, 'images')
+        if os.listdir(output_image_path):
+            exist_images = True
 
-        # 获取图片的父路径，为的是以相对路径保存到 .md 和 conent_list.json 文件中
-        image_path_parent = os.path.basename(output_image_path)
         md_path = os.path.join(output_path, f"{pdf_name}.md")
         with open(md_path, "r", encoding="utf-8") as f:
             result = f.read()
+            print(result)
+        # 如果路径中存在图片 则1,向minio传入文件 2,调用多模态服务
     except Exception as e:
         print(f"Error in post_pdf_parse_main: {str(e)}")
     file_info.content = result
-    return file_info
+    return file_info, exist_images
+
 
 async def process_files_background(file_list: FileList, pdf_content: bytes):
+    callback_data = dict()
+    exist_images = False
     try:
         processed_files = []
         for file_info in file_list.file_list:
             pdf_path = await write_pdf_stream_to_file(file_list, pdf_content)
-            # pdf_parse_main(pdf_path) # 同步阻塞
-            processed_file = await post_pdf_parse_main(file_info, pdf_path)
+            # pdf_parse_main(pdf_path)  # 同步阻塞
+            processed_file, exist_images = await post_pdf_parse_main(file_info, pdf_path)
             processed_files.append(processed_file)
-
         callback_data = {
             "code": 200,
             "msg": "success",
@@ -142,11 +153,9 @@ async def process_files_background(file_list: FileList, pdf_content: bytes):
                 "token": file_list.token
             }
         }
-
-        await send_callback(callback_data)
     except Exception as e:
         print(f"Error in background processing: {str(e)}")
-        error_data = {
+        callback_data = {
             "code": 500,
             "msg": f"Error: {str(e)}",
             "data": {
@@ -154,10 +163,37 @@ async def process_files_background(file_list: FileList, pdf_content: bytes):
                 "token": file_list.token
             }
         }
-        await send_callback(error_data)
+    finally:
+        if file_list.strategy == "fast":
+            # directly_back 如果fast模式 此处不考虑图片描述信息 直接回调langchain
+            await send_callback(callback_data)
+        else:
+            # 判断是否存在图片 - 不存在 -> 直接回调langchain - 存在 -> 调用下游多模态服务
+            if not exist_images:
+                await send_callback(callback_data)
+            # call multi_model
+            else:
+                pass
 
 
-async def send_callback(data: dict):
+async def call_multi_model(file_list: FileList):
+    # 压缩md文件,images图片包 stream传输
+
+    try:
+        with open(self.file_path, 'rb') as file:
+            files = {
+                'file': (md_file_name, file, 'application/pdf'),
+            }
+            response = requests.request("POST", url, data=body, files=files)
+        if response.status_code == 200:
+            result = ""
+    except Exception as e:
+        import traceback
+        msg = traceback.format_exc()
+        logging.error(msg)
+    return result
+
+async def send_callback(data: dict) -> None:
     async with aiohttp.ClientSession() as session:
         try:
             async with session.post(CALLBACK_URL, json=data) as response:
